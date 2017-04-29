9c202ab1d65b499b0d9ebf7c2a9405a0e10b3f9e
Arnold Okoth <arnoldokoth@Andelas-MacBook-Pro.local>
2017-04-29 16:10:33 +0300

    just rolled an update

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def hello_world():
     return "Hello, World!"
 
+
+@app.route('/update', methods=['GET'])
+def update():
+    return "Just Rolled An Update"
+
+
 if __name__ == '__main__':
     app.run(port=8000, host='0.0.0.0')
