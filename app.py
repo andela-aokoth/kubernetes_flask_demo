18bbcd23b94dc598e1a1c25bad4221a1d0dd4079
arnoldokoth <arnold.okoth@andela.com>
2017-05-17 17:23:04 +0300

    random update to deploy to elastic beanstalk

--- app.py
+++ app.py
@@ -12,5 +12,10 @@ def update():
     return "Just Rolled An Update"
 
 
+@app.route('/another', methods=['GET'])
+def another():
+    return "This is another update"
+
+
 if __name__ == '__main__':
     app.run(port=8000, host='0.0.0.0')
